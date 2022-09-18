c0755c82544bc6241dcb1de9133599a4a4ed1a11
MiriamFernandezPerez <mirindaww@hotmail.com>
2022-09-18 21:28:04 +0200

    Ejercicio 4

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ function request(){
         answerOK = answer.joke;
         console.log(answerOK);
         document.querySelector("#textJoke").innerHTML = answerOK;
-    });
+    })
+    .catch(err => console.error(err));
     document.getElementById("score").style.visibility = "visible";
 }
 
@@ -36,3 +37,25 @@ function reportJoke(value){
     console.log(reportJokes);
     document.getElementById("score").style.visibility = "hidden";
 }
+
+//API WEATHER
+const weatherUrl = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=40.4195&lon=-3.7034';
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6c21a4a72dmshfad1c7af2660b8dp13f930jsn0f1ab4693026',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
+};
+
+let weatherStatus;
+
+fetch(weatherUrl, options2)
+	.then(response => response.json())
+	.then(response =>{
+        weatherStatus = response.data[0].weather.description;
+        // console.log(response);
+        console.log(weatherStatus);
+        document.querySelector("#weather").innerHTML = weatherStatus;
+    })
+	.catch(err => console.error(err));
